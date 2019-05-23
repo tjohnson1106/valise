ca6ccaafd0fae667af64c447da2cc9b9c395fa4d
Thomas Johnson <tjohnson1106@gmail.com>
2019-05-23 16:36:35 -0400

    added room  search flter in context

--- src/context.js
+++ src/context.js
@@ -105,6 +105,22 @@ class RoomProvider extends Component {
     //  filter by price
     tempRooms = tempRooms.filter((room) => room.price <= price);
 
+    // filter by size
+    tempRooms = tempRooms.filter(
+      (room) => room.size >= minSize && room.size <= maxSize
+    );
+
+    // filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter((room) => room.breakfast === true);
+    }
+
+    // filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter((room) => room.pets === true);
+    }
+
+    // change state
     this.setState({
       sortedRooms: tempRooms
     });
